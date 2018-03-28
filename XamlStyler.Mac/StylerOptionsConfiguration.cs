c6a9b71acf71a235e50f584fdc5e7a6cebdd2747
Nico Vermeir <nicovermeir@Nicos-MacBook-Pro.local>
2018-03-28 08:01:31 +0200

    initial VS4Mac version

---
+++ XamlStyler.Mac/StylerOptionsConfiguration.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using MonoDevelop.Core;
+using Newtonsoft.Json;
+using Xavalon.XamlStyler.Core.Options;
+
+namespace Xavalon.XamlStyler.Mac
+{
+	public static class StylerOptionsConfiguration
+	{
+		public static StylerOptions ReadFromUserProfile()
+		{
+			var filePath = GetOptionsFilePath().ToString();
+
+			try
+			{
+				var text = File.ReadAllText(filePath);
+				return JsonConvert.DeserializeObject<StylerOptions>(text);
+			}
+			catch (FileNotFoundException)
+			{
+			}
+			catch (Exception ex)
+			{
+				LoggingService.LogError("Exception when saving user XamlStyler options", ex);
+
+				// delete file on any other exception (malformed etc.)
+				File.Delete(filePath);
+			}
+
+			// Xamarin Forms defaults
+			var options = new StylerOptions()
+			{
+				IndentSize = 4,
+			};
+
+			try
+			{
+				// update attribute ordering to include Forms attrs
+				options.AttributeOrderingRuleGroups[6] += ", WidthRequest, HeightRequest";
+				options.AttributeOrderingRuleGroups[7] += ", HorizontalOptions, VerticalOptions, XAlign, VAlign";
+			}
+			catch (Exception ex)
+			{
+				LoggingService.LogError("Exception when updating default options to include Xamarin Forms attributes", ex);
+			}
+
+			return options;
+		}
+
+		public static void WriteToUserProfile(StylerOptions options)
+		{
+			try
+			{
+				var text = JsonConvert.SerializeObject(options);
+				File.WriteAllText(GetOptionsFilePath().ToString(), text);
+			}
+			catch (Exception ex)
+			{
+				LoggingService.LogError("Exception when saving user XamlStyler options", ex);
+			}
+		}
+
+		public static void Reset()
+		{
+			File.Delete(GetOptionsFilePath().ToString());
+		}
+
+		private static FilePath GetOptionsFilePath()
+		{
+			return UserProfile.Current.ConfigDir.Combine("xamlstyler.config");
+		}
+	}
+}
---
+++ XamlStyler.Mac_old/FormatXamlHandler.cs
@@ -0,0 +1,46 @@
+﻿using MonoDevelop.Components.Commands;
+using MonoDevelop.Core;
+using MonoDevelop.Ide;
+using Xavalon.XamlStyler.Core;
+
+namespace Xavalon.XamlStyler.XamarinStudio
+{
+	public class FormatXamlHandler : CommandHandler
+	{
+		protected override void Run()
+		{
+			var options = StylerOptionsConfiguration.ReadFromUserProfile();
+			var styler = new StylerService(options);
+
+			var doc = IdeApp.Workbench.ActiveDocument;
+			var edit = doc.Editor;
+
+			if (edit != null)
+			{
+				var styledXaml = styler.StyleDocument(edit.Text);
+
+				using (edit.OpenUndoGroup())
+				{
+					edit.RemoveText(0, edit.Text.Length);
+					edit.InsertText(0, styledXaml);
+				}
+				doc.IsDirty = true;
+			}
+		}
+
+		protected override void Update(CommandInfo info)
+		{
+			var doc = IdeApp.Workbench.ActiveDocument;
+			if (doc != null || doc.FileName.Extension.ToLowerInvariant() == ".xaml")
+			{
+				LoggingService.LogInfo($"XamlStyler: Filename is {doc.FileName}, extension is ENABLED");
+				info.Enabled = info.Visible = true;
+			}
+			else
+			{
+				LoggingService.LogInfo($"XamlStyler: Filename is {doc.FileName}, extension is DISABLED");
+				info.Visible = false;
+			}
+		}
+	}
+}
